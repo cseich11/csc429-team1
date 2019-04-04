9fc35a6d23479f17612f09e46af31820c4defd76
Torrin Lisi <torrinlisi@gmail.com>
2019-04-04 17:37:23 -0400

    Create SearchVendorAction.java

---
+++ TermProjectTeam1/src/model/SearchVendorAction.java
@@ -0,0 +1,135 @@
+// specify the package
+package model;
+
+// system imports
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import java.util.Properties;
+import java.util.Vector;
+
+// project imports
+import event.Event;
+import exception.InvalidPrimaryKeyException;
+
+import userinterface.View;
+import userinterface.ViewFactory;
+
+//==============================================================
+public class SearchVendorAction extends Action
+{
+	// GUI Components
+
+	private String actionErrorMessage = "";
+	
+	private String vName;
+	private String vPhone;
+	private String vStatus;
+	
+	private Vendor v;
+	
+	private VendorCollection vc;
+	
+
+	
+	//----------------------------------------------------------
+	public SearchVendorAction()
+		throws Exception
+	{
+		super();
+	}
+
+	//----------------------------------------------------------
+	protected void setDependencies()
+	{
+		dependencies = new Properties();
+		dependencies.setProperty("SearchVendor", "ActionError");
+		dependencies.setProperty("Cancel", "CancelAction");
+		dependencies.setProperty("OK", "CancelAction");
+		dependencies.setProperty("VendorData", "UpdateStatusMessage");
+
+		myRegistry.setDependencies(dependencies);
+	}
+
+
+	
+	//----------------------------------------------------------
+	public void processAction(Properties props)
+	{
+
+		vName = props.getProperty("vName");
+		vPhone = props.getProperty("vPhone");
+		
+		vc = new VendorCollection(vName,vPhone);
+		
+	}
+
+
+	//-----------------------------------------------------------
+	public Object getState(String key)
+	{
+		if (key.equals("ActionError") == true)
+		{
+			return actionErrorMessage;
+		}
+//		else
+//		if (key.equals("UpdateStatusMessage") == true)
+//		{
+//			return addVendorStatusMessage;
+//		}
+		else if(key.equals("SearchVendor"))
+		{
+			String[] vData = {vName, vPhone};
+			return vData;
+		}
+		
+		return null;
+	}
+
+	//-----------------------------------------------------------
+	public void stateChangeRequest(String key, Object value)
+	{
+		if (key.equals("DoYourJob") == true)
+			doYourJob();
+		else if (key.equals("VendorData") == true)
+			processAction((Properties)value);
+		
+
+		myRegistry.updateSubscribers(key, this);
+	}
+
+
+	//------------------------------------------------------
+	protected Scene createView()
+	{
+
+		Scene currentScene = myViews.get("AddVendorActionView");
+
+		if (currentScene == null)
+		{
+			// create our initial view
+			View newView = ViewFactory.createView("AddVendorActionView", this);
+			currentScene = new Scene(newView);
+			myViews.put("AddVendorActionView", currentScene);
+
+			return currentScene;
+		}
+		else
+		{
+			return currentScene;
+		}
+	}
+
+
+	//------------------------------------------------------
+	protected void createAndShowView()
+	{
+		View newView = ViewFactory.createView("SearchVendorActionView", this);
+		Scene newScene = new Scene(newView);
+
+		myViews.put("SearchVendorActionView", newScene);
+
+		// make the view visible by installing it into the stage
+		swapToView(newScene);
+	}
+
+}
