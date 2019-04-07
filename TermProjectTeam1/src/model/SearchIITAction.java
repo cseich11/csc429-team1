0e4ead8e5213a8f2233ce20decc88af61eba47cf
jakethurnau <jthurnau98@gmail.com>
2019-04-07 19:59:53 -0400

    trying to get update messages

--- TermProjectTeam1/src/model/SearchIITAction.java
+++ TermProjectTeam1/src/model/SearchIITAction.java
@@ -110,7 +110,7 @@ public class SearchIITAction extends Action
 		if (key.equals("ActionError"))
 			return actionErrorMessage;
 		if(key.equals("UpdateStatusMessage"))
-			return inventoryUpdateStatusMessage;
+			return iitUpdateStatusMessage;
 		if(key.equals("InventoryItemTypeList"))
 			return list;
 		if(key.equals("IITData"))
@@ -161,6 +161,9 @@ public class SearchIITAction extends Action
 			iit.persistentState.setProperty("Status", "Inactive");
 			iit.update();
 			iitUpdateStatusMessage = (String)iit.getState("UpdateStatusMessage");
+			list = new InventoryItemTypeCollection();
+			list.findAllIITWithNameNotes(itemTypeNameSearched, notesSearched);
+			createAndShowIITListView();
 		}
 		else if (key.equals("IITData") == true)
 			processAction((Properties)value);
