f352e72b2f292e8405ea9bd8fa5861d6f10a2ef5
jakethurnau <jthurnau98@gmail.com>
2019-04-18 17:51:26 -0400

    classes to help implement deleting VIIT

---
+++ TermProjectTeam1/src/model/SearchVIITAction.java
@@ -0,0 +1,233 @@
+// specify the package
+package model;
+
+// system imports
+import javafx.scene.Scene;
+import java.util.Properties;
+
+import exception.InvalidPrimaryKeyException;
+import userinterface.View;
+import userinterface.ViewFactory;
+
+/** The class containing the SearchInventoryItemTypesAction for the Library application */
+//==============================================================
+public class SearchVIITAction extends Action
+{
+	private InventoryItemTypeCollection list; 
+
+	// GUI Components
+	private String itemTypeName, notes, units, unitMeasure, validityDays, reorderPoint, status, itemTypeNameSearched, notesSearched;
+	private String actionErrorMessage = "";
+	private String inventoryUpdateStatusMessage = "";
+	private String iitUpdateStatusMessage = "";
+	
+	private InventoryItemType iit;
+	
+	/**
+	 * Constructor for this class.
+	 *
+	 *
+	 */
+	//----------------------------------------------------------
+	public SearchVIITAction()
+		throws Exception
+	{
+		super();
+	}
+
+	//----------------------------------------------------------
+	protected void setDependencies()
+	{
+		dependencies = new Properties();
+		dependencies.setProperty("Cancel", "CancelAction");
+		dependencies.setProperty("SearchIIT", "ActionError");
+		dependencies.setProperty("OK", "CancelAction");
+		dependencies.setProperty("ModifyIITData", "ActionMessage");
+		dependencies.setProperty("IITData", "UpdateStatusMessage");
+
+		myRegistry.setDependencies(dependencies);
+	}
+
+	/**
+	 * This method encapsulates all the logic of creating the account,
+	 * verifying ownership, crediting, etc. etc.
+	 */
+	//----------------------------------------------------------
+	public void processAction(String[] data)
+	{
+		if(data.length == 2 && data[0] != null && data[1] != null)
+		{
+			list = new InventoryItemTypeCollection();
+			itemTypeNameSearched = data[0]; notesSearched = data[1];
+			list.findAllIITWithNameNotes(itemTypeNameSearched, notesSearched);
+			createAndShowIITListView();
+		}
+//		if (props.getProperty("author") != null && props.getProperty("title") != null
+//				&& props.getProperty("pubYear") != null && props.getProperty("status") != null)
+//		{
+//			book = new InventoryItemType(props);
+//			book.update();
+//			libraryUpdateStatusMessage = (String)book.getState("UpdateStatusMessage");
+//			//createAndShowReceiptView();
+//
+//		}
+	}
+	
+	/**
+	 * This method encapsulates all the logic of updating the iit
+	 */
+	//----------------------------------------------------------
+	public void processAction(Properties props)
+	{
+
+		itemTypeName = props.getProperty("ItemTypeName");
+		units = props.getProperty("Units");
+		unitMeasure = props.getProperty("UnitMeasure");
+		validityDays = props.getProperty("ValidityDays");
+		reorderPoint = props.getProperty("ReorderPoint");
+		notes = props.getProperty("Notes");
+		status = props.getProperty("Status");
+
+		if(itemTypeName != null && units != null && unitMeasure != null && validityDays!= null && reorderPoint != null && notes != null && status != null)
+		{
+			iit.persistentState.setProperty("Units", units);
+			iit.persistentState.setProperty("UnitMeasure", unitMeasure);
+			iit.persistentState.setProperty("ValidityDays", validityDays);
+			iit.persistentState.setProperty("ReorderPoint", reorderPoint);
+			iit.persistentState.setProperty("Notes", notes);
+			iit.persistentState.setProperty("Status", status);
+			iit.update();
+			inventoryUpdateStatusMessage = (String)iit.getState("UpdateStatusMessage");
+			//list = new InventoryItemTypeCollection();
+			//list.findAllIITWithNameNotes(itemTypeNameSearched, notesSearched);
+			//createAndShowIITListView();
+		}
+	}
+
+	//-----------------------------------------------------------
+	public Object getState(String key)
+	{
+		if (key.equals("ActionError"))
+			return actionErrorMessage;
+		if (key.equals("ActionMessage"))
+			return inventoryUpdateStatusMessage;
+		if(key.equals("UpdateStatusMessage"))
+			return iitUpdateStatusMessage;
+		if(key.equals("InventoryItemTypeList"))
+			return list;
+		if(key.equals("IITData"))
+		{
+			String[] iitData = {itemTypeName, units, unitMeasure, validityDays, reorderPoint, notes, status};
+			return iitData;
+		}
+		if (iit != null)
+			return iit.getState(key);
+		return null;
+	}
+
+	//-----------------------------------------------------------
+	public void stateChangeRequest(String key, Object value)
+	{
+		// DEBUG System.out.println("SearchInventoryItemTypesAction.sCR: key: " + key);
+
+		if(key.equals("DoYourJob"))
+			doYourJob();
+		else if(key.equals("IITData"))
+			processAction((String[])value);
+		else if(key.equals("CancelInventoryItemTypeList"))
+			swapToView(createView());
+		else if(key.equals("ModifyIIT")) {
+			try {
+				iit = new InventoryItemType((String)value);
+			} catch (InvalidPrimaryKeyException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+//			createAndShowModifyVIITView();
+		}
+		else if(key.equals("ModifyIITData"))
+			processAction((Properties)value);
+		else if(key.equals("ConfirmDeleteIIT")) {
+			try {
+				iit = new InventoryItemType((String)value);
+			} catch (InvalidPrimaryKeyException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			createAndShowDeleteVIITView();
+		}
+		else if(key.equals("CancelModify"))
+			createAndShowIITListView();
+		else if(key.equals("Delete"))
+		{
+			iit.persistentState.setProperty("Status", "Inactive");
+			iit.update();
+			iitUpdateStatusMessage = (String)iit.getState("UpdateStatusMessage");
+			list = new InventoryItemTypeCollection();
+			list.findAllIITWithNameNotes(itemTypeNameSearched, notesSearched);
+			createAndShowIITListView();
+		}
+		else if (key.equals("VIITData") == true)
+			processAction((Properties)value);
+
+		myRegistry.updateSubscribers(key, this);
+	}
+	
+	/** Called via the IView relationship */
+	//----------------------------------------------------------
+	public void updateState(String key, Object value)
+	{
+		stateChangeRequest(key, value);
+	}
+
+	
+//	public void deleteIIT()
+//	{
+//		iit.persistentState.setProperty("Status", "Inactive");
+//		iit.update();
+//	}
+	
+	/**
+	 * Create the view of this class. And then the super-class calls
+	 * swapToView() to display the view in the frame
+	 */
+	//------------------------------------------------------
+	protected Scene createView()
+	{
+		Scene currentScene = myViews.get("SearchVIITActionView");
+
+		if (currentScene == null)
+		{
+			// create our initial view
+			View newView = ViewFactory.createView("SearchVIITActionView", this);
+			currentScene = new Scene(newView);
+			myViews.put("SearchVIITActionView", currentScene);
+
+			return currentScene;
+		}
+		else
+		{
+			return currentScene;
+		}
+	}
+	
+	protected void createAndShowIITListView()
+	{
+		View newView = ViewFactory.createView("VendorInventoryItemTypeCollectionView", this);
+		Scene currentScene = new Scene(newView);
+		myViews.put("VendorInventoryItemTypeCollectionView", currentScene);
+
+		swapToView(currentScene);
+	}
+	
+	
+	protected void createAndShowDeleteVIITView()
+	{
+		View newView = ViewFactory.createView("DeleteVIITActionView", this);
+		Scene currentScene = new Scene(newView);
+		myViews.put("DeleteVIITActionView", currentScene);
+
+		swapToView(currentScene);
+	}
+
+}
---
+++ TermProjectTeam1/src/userinterface/DeleteVIITActionView.java
@@ -0,0 +1,207 @@
+// specify the package
+package userinterface;
+
+// system imports
+import javafx.event.Event;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
+
+import java.util.Properties;
+
+// project imports
+import impresario.IModel;
+
+
+//==============================================================
+public class DeleteVIITActionView extends View
+{
+
+	// Model
+
+	// GUI components
+	private TextField itemTypeNameSearch;
+
+	private Button submitButton;
+	private Button cancelButton;
+
+	// For showing error message
+	private MessageView statusLog;
+
+	// constructor for this class -- takes a model object
+	//----------------------------------------------------------
+	public DeleteVIITActionView(IModel action)
+	{
+		super(action, "DeleteVIITActionView");
+
+		// create a container for showing the contents
+		VBox container = new VBox(10);
+		container.setPadding(new Insets(15, 5, 5, 5));
+
+		// create our GUI components, add them to this panel
+		container.getChildren().add(createTitle());
+		container.getChildren().add(createFormContent());
+
+		// Error message area
+		container.getChildren().add(createStatusLog("                          "));
+
+		getChildren().add(container);
+
+		//populateFields();
+		
+		myModel.subscribe("UpdateStatusMessage", this);
+	}
+
+
+	// Create the label (Text) for the title
+	//-------------------------------------------------------------
+	private Node createTitle()
+	{
+		
+		Text titleText = new Text("       Brockport Restaurant          ");
+		titleText.setWrappingWidth(300);
+		titleText.setFont(Font.font("Arial", FontWeight.BOLD, 20));
+		titleText.setTextAlignment(TextAlignment.CENTER);
+		titleText.setFill(Color.DARKGREEN);
+		
+		return titleText;
+	}
+
+	// Create the main form content
+	//-------------------------------------------------------------
+	private VBox createFormContent()
+	{
+		VBox vbox = new VBox(10);
+
+		GridPane grid = new GridPane();
+        	grid.setAlignment(Pos.CENTER);
+       		grid.setHgap(10);
+        	grid.setVgap(10);
+        	grid.setPadding(new Insets(25, 25, 25, 25));
+
+		Label confirmDeleteLabel = new Label("Are you sure you want to delete this Vendor-Inventory-Item-Type?");
+		grid.add(confirmDeleteLabel, 0, 0);
+
+		submitButton = new Button("Confirm");
+ 		submitButton.setOnAction(e -> {
+ 			clearErrorMessage(); 
+			// do the delete
+			processAction(e);
+        });
+
+		cancelButton = new Button("Cancel");
+ 		cancelButton.setOnAction(e -> {
+ 			clearErrorMessage();
+			myModel.stateChangeRequest("Cancel", null); 
+        });
+
+		HBox btnContainer = new HBox(100);
+		btnContainer.setAlignment(Pos.CENTER);
+		btnContainer.getChildren().add(submitButton);
+		btnContainer.getChildren().add(cancelButton);
+
+		vbox.getChildren().add(grid);
+		vbox.getChildren().add(btnContainer);
+
+		return vbox;
+	}
+
+	// Create the status log field
+	//-------------------------------------------------------------
+	private MessageView createStatusLog(String initialMessage)
+	{
+		statusLog = new MessageView(initialMessage);
+
+		return statusLog;
+	}
+
+	//-------------------------------------------------------------
+	//public void populateFields()
+	{
+	}
+
+	// process events generated from our GUI components
+	//-------------------------------------------------------------
+	public void processAction(Event evt)
+	{
+		// DEBUG: System.out.println("DepositAmountView.processAction()");
+
+		clearErrorMessage();
+
+		//String itemTypeNameEntered = itemTypeNameSearch.getText();
+		
+//		if(titleEntered == null || titleEntered.length() == 0)
+//			displayErrorMessage("Please enter a title");
+//		else
+//		{
+			//processData(itemTypeNameEntered);
+			myModel.stateChangeRequest("Delete", null);
+			
+//		}
+	}
+
+	/**
+	 * Process amount entered by user.
+	 * Action is to pass this info on to the action object.
+	 */
+	//----------------------------------------------------------
+	private void processData(String itn)
+	{
+		myModel.stateChangeRequest("IIT Data", itn);
+	}
+
+	
+
+	/**
+	 * Required by interface
+	 */
+	//---------------------------------------------------------
+	public void updateState(String key, Object value)
+	{
+		if(key.equals("UpdateStatusMessage"))
+		{
+			String msg = (String)value;
+			displayMessage(msg);
+		}
+	}
+
+	/**
+	 * Display error message
+	 */
+	//----------------------------------------------------------
+	public void displayErrorMessage(String message)
+	{
+		statusLog.displayErrorMessage(message);
+	}
+	
+	/**
+	 * Display message
+	 */
+	//----------------------------------------------------------
+	public void displayMessage(String message)
+	{
+		statusLog.displayMessage(message);
+	}
+
+	/**
+	 * Clear error message
+	 */
+	//----------------------------------------------------------
+	public void clearErrorMessage()
+	{
+		statusLog.clearErrorMessage();
+	}
+
+}
