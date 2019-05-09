3b551b7d0688f97a90f7afa11687609425825186
Casey Eich <cseich11@gmail.com>
2019-05-09 17:29:50 -0400

    Revert "ModifyIIStatus classes"
    This reverts commit b89cde59558d5fbd2e8f5ea1a4a39fc19001d70e.

--- TermProjectTeam1/src/model/SearchIIAction.java
+++ TermProjectTeam1/src/model/SearchIIAction.java
@@ -88,23 +88,15 @@ public class SearchIIAction extends Action
 		{
 			try {
 				ii = new InventoryItem((String)value);
-					createAndShowSearchIIChoiceView();
+				if(ii.getStatus().equals("Available"))
+					createAndShowDeleteIIView();
+				else
+					actionErrorMessage = "Inventory Item does not exist";
 			} catch (InvalidPrimaryKeyException e) {
 				actionErrorMessage = "Inventory Item does not exist";
 			}
 			
 		}
-		else if(key.equals("ModifyIIStatus"))
-		{
-			try {
-				ii = new InventoryItem((String)value);
-					//createAndShowModifyIIStatusView();
-			} catch (InvalidPrimaryKeyException e) {
-				actionErrorMessage = "Inventory Item does not exist";
-			}
-			
-		}
-		
 		else if(key.equals("IIDelete")) {
 			processActionDelete((Properties)value);
 			swapToView(createView());
@@ -181,14 +173,4 @@ public class SearchIIAction extends Action
 
 		swapToView(currentScene);
 	}
-	
-	protected void createAndShowSearchIIChoiceView()
-	{
-		View newView = ViewFactory.createView("SearchIIChoiceView", this);
-		Scene currentScene = new Scene(newView);
-		myViews.put("SearchIIChoiceView", currentScene);
-
-		swapToView(currentScene);
-	}
-	
 }
--- TermProjectTeam1/src/userinterface/ViewFactory.java
+++ TermProjectTeam1/src/userinterface/ViewFactory.java
@@ -42,15 +42,8 @@ public class ViewFactory {
 			return new VendorCollectionForVIITView(model);
 		if(viewName.equals("ConfirmDeleteVIITView"))
 			return new ConfirmDeleteVIITView(model);
-		if(viewName.equals("ModifyIIStatusView"))
-			return new ModifyIIStatusView(model);
-		if(viewName.equals("SearchIIChoiceView"))
-			return new SearchIIChoiceView(model);
 		if(viewName.equals("PriceView"))
 			return new PriceView(model);
-		
-		
-		
 		return null;
 	}
 }
